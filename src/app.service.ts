7a120c9431561291a9b32b478b770c6ba63d1c14
yousafsabir <myousafdev@gmail.com>
2025-07-19 11:44:18 +0500

    feat: setup cron

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  ping() {
+    console.log('Ping...');
   }
 }
